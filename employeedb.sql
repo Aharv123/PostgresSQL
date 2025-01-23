8834eb09d50cc8fa8d116d94278833dac2c38f62
Atharv Salunke <atharvsalunke@gmail.com>
2025-01-23 18:19:34 +0530

    Add files via upload

--- employeedb.sql
+++ employeedb.sql
@@ -1,7 +1,12 @@
-
-
-
-
-SELECT department, COUNT(*) FROM employee GROUP BY department;
-
-
+SELECT 
+    employee.name, 
+    employee.department, 
+    employee.salary, 
+    employee.date, 
+    employee_t2.experience_yr
+FROM 
+    employee
+FULL OUTER JOIN 
+    employee_t2
+ON 
+    employee.id = employee_t2.experience_yr;
