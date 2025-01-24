0e32fdb9aaf6f6cca0e223a9be8a7c2f09ad67b4
Atharv Salunke <atharvsalunke@gmail.com>
2025-01-24 19:40:52 +0530

    Add files via upload

--- employeedb.sql
+++ employeedb.sql
@@ -1,12 +1,1 @@
-SELECT 
-    employee.name, 
-    employee.department, 
-    employee.salary, 
-    employee.date, 
-    employee_t2.experience_yr
-FROM 
-    employee
-FULL OUTER JOIN 
-    employee_t2
-ON 
-    employee.id = employee_t2.experience_yr;
+select * from employee;
